945c2a0f5c923928506d9ea860d15a6b52dec606
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2022-06-02 16:22:37 +0100

    feat: avoid duplicated entries on faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,46 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import { envConfig } from "config/env.config";
+import { query as q } from 'faunadb';
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import { envConfig } from 'config/env.config';
+import { fauna } from 'services/fauna';
 
 export default NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: envConfig.auth.github.id,
-      clientSecret: envConfig.auth.github.secret,
-      authorization: { params: { scope: "read:user" } },
-    }),
-  ],
-  secret: envConfig.auth.secret,
+	providers: [
+		GithubProvider({
+			clientId: envConfig.auth.github.id,
+			clientSecret: envConfig.auth.github.secret,
+			authorization: { params: { scope: 'read:user' } },
+		}),
+	],
+	secret: envConfig.auth.secret,
+	callbacks: {
+		async signIn({ user, account, profile }) {
+			try {
+				await fauna.query(
+					q.If(
+						q.Not(
+							q.Exists(
+								q.Match(
+									q.Index('user_by_email'),
+									q.Casefold(user.email!)
+								)
+							)
+						),
+						q.Create(q.Collection('users'), {
+							data: { email: user.email },
+						}),
+						q.Get(
+							q.Match(
+								q.Index('user_by_email'),
+								q.Casefold(user.email!)
+							)
+						)
+					)
+				);
+				return true;
+			} catch {
+				return false;
+			}
+		},
+	},
 });

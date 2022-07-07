f8a633715ef14390333f9429827a621de105a6dc
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2022-07-07 19:19:17 +0100

    feat: change paths

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -1,31 +1,31 @@
-import { createStitches } from "@stitches/react";
-import { colors } from "styles/partials/colors";
-import { font } from "styles/partials/font";
-import { lineHeights } from "styles/partials/line-height";
-import { utils } from "styles/partials/utils";
-import { radii } from "styles/partials/radii";
-import { sizes } from "styles/partials/sizes";
-import { space } from "styles/partials/space";
-import { media } from "styles/partials/media";
+import { createStitches } from '@stitches/react';
+import { colors } from '~/styles/partials/colors';
+import { font } from '~/styles/partials/font';
+import { lineHeights } from '~/styles/partials/line-height';
+import { utils } from '~/styles/partials/utils';
+import { radii } from '~/styles/partials/radii';
+import { sizes } from '~/styles/partials/sizes';
+import { space } from '~/styles/partials/space';
+import { media } from '~/styles/partials/media';
 
 export const {
-  styled,
-  css,
-  globalCss,
-  keyframes,
-  getCssText,
-  theme,
-  createTheme,
-  config,
+	styled,
+	css,
+	globalCss,
+	keyframes,
+	getCssText,
+	theme,
+	createTheme,
+	config,
 } = createStitches({
-  theme: {
-    colors,
-    sizes,
-    space,
-    ...font,
-    lineHeights,
-    radii,
-  },
-  media,
-  utils,
+	theme: {
+		colors,
+		sizes,
+		space,
+		...font,
+		lineHeights,
+		radii,
+	},
+	media,
+	utils,
 });

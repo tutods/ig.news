25f23876c2ac82b45a5e614f8b2ed04ef16572a1
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2022-07-07 22:08:23 +0100

    chore: remove commented code

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,43 +1,37 @@
-import { globalCss } from "@stitches/react";
+import { globalCss } from '@stitches/react';
 
 export const globalStyles = globalCss({
-  "@import": [
-    "url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap')",
-  ],
+	// Import font from Google Fonts
+	'@import': [
+		"url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap')",
+	],
 
-  // "@font-face": [
-  //     {
-  //         fontFamily: '"Roboto", sans-serif',
-  //         src: "url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap')",
-  //     },
-  // ],
+	'*': {
+		padding: 0,
+		margin: 0,
+		boxSizing: 'border-box',
+	},
 
-  "*": {
-    padding: 0,
-    margin: 0,
-    boxSizing: "border-box",
-  },
+	button: {
+		border: 0,
+		boxShadow: 'none',
+		cursor: 'pointer',
+		color: '$text',
+	},
 
-  button: {
-    border: 0,
-    boxShadow: "none",
-    cursor: "pointer",
-    color: "$text",
-  },
+	a: { color: 'inherit', textDecoration: 'none' },
 
-  a: { color: "inherit", textDecoration: "none" },
+	body: {
+		backgroundColor: '$background',
+		color: '$text',
 
-  body: {
-    backgroundColor: "$background",
-    color: "$text",
+		fontFamily: '$body',
+		fontWeight: '$medium',
+		fontSize: '$16',
+		lineHeight: '$24',
+	},
 
-    fontFamily: "$body",
-    fontWeight: "$medium",
-    fontSize: "$16",
-    lineHeight: "$24",
-  },
-
-  "h1,h2,h3,h4,h5,h6": {
-    color: "$white",
-  },
+	'h1,h2,h3,h4,h5,h6': {
+		color: '$white',
+	},
 });
